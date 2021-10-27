b4ab09f5a830ca9d5ee9f0e9ed381f6b56c16f1c
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2021-10-26 21:27:49 -0300

    createusercase
    createusercase

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,15 +1,17 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 
+@injectable()
 class CreateUserUseCase {
     constructor(
         @inject("UsersRepository")
         private usersRepository: IUsersRepository
     ) {}
 
-    async execute({name,
+    async execute({
+        name,
         username,
         email,
         password,
